c43ac630b762690465b5a9c85e11bc99a01b9df7
Rinadfahadz <rinad-alzuaybir@outlook.com>
2025-08-30 15:20:10 +0000

    updates

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -5,3 +5,13 @@ int _putchar(char c);
 void print_alphabet(void);
 
 #endif /* MAIN_H */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+
+#endif /* MAIN_H */
+
