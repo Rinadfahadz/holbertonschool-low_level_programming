59449548ed180ca83cf35fe6337f11c82f079986
Rinadfahadz <rinad-alzuaybir@outlook.com>
2025-09-20 18:24:10 +0000

    update

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -12,25 +12,23 @@
 char *_strdup(char *str)
 {
 	char *dup;
-	unsigned int i, len = 0;
+	unsigned int i;
+	unsigned int len = 0;
 
 	if (str == NULL)
 		return (NULL);
 
-	/* احسب طول السلسلة */
 	while (str[len] != '\0')
 		len++;
 
-	/* احجز الذاكرة: طول السلسلة + 1 للـ null terminator */
 	dup = malloc(sizeof(char) * (len + 1));
 	if (dup == NULL)
 		return (NULL);
 
-	/* انسخ الأحرف */
 	for (i = 0; i < len; i++)
 		dup[i] = str[i];
+
 	dup[len] = '\0';
 
 	return (dup);
 }
-
