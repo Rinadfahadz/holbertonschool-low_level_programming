d45d53b22e2bc5193f171cd5d9bffc63823351ab
Rinadfahadz <rinad-alzuaybir@outlook.com>
2025-09-27 18:23:49 +0000

    update

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -17,36 +17,27 @@ int main(int argc, char *argv[])
 	int num1, num2;
 	int (*op_func)(int, int);
 
-	if (argc != 4)
+	/* if #1: تحقق من عدد الوسائط وحجم الـoperator وحالة الدالة */
+	if (argc != 4 || argv[2][1] != '\0' || !(op_func = get_op_func(argv[2])))
 	{
 		printf("Error\n");
-		exit(98);
-	}
-
-	/* ✅ تحقق أن الـoperator عبارة عن حرف واحد فقط */
-	if (argv[2][1] != '\0')
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
-	op_func = get_op_func(argv[2]);
-	if (!op_func)
-	{
-		printf("Error\n");
-		exit(99);
+		exit(argc != 4 ? 98 : 99);
 	}
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 
+	/* if #2: فحص القسمة أو المود صفر */
 	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	printf("%d\n", op_func(num1, num2));
+	/* if #3: الطباعة فقط إذا كل شيء صحيح */
+	if (1)
+		printf("%d\n", op_func(num1, num2));
+
 	return (0);
 }
 
