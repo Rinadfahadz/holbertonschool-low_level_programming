6f7975dcb5ca2b353a9a18c14c77a64913a9d528
Rinadfahadz <rinad-alzuaybir@outlook.com>
2025-09-27 18:22:38 +0000

    update

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -2,28 +2,29 @@
 #include <stddef.h>
 
 /**
- * get_op_func - selects the correct function to perform
- *               the operation asked by the user
- * @s: operator passed as argument to the program
+ * get_op_func - select the correct operation
+ * @s: operator string
  *
- * Return: pointer to the function that corresponds
- *         to the operator, or NULL if no match
+ * Return: pointer to matching function or NULL
  */
 int (*get_op_func(char *s))(int, int)
 {
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i = 0;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
 
-    while (ops[i].op != NULL && *(ops[i].op) != *s)
-        i++;
-
-    return (ops[i].f);
+	while (ops[i].op)
+	{
+		if (s[1] == '\0' && *(ops[i].op) == *s)
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
 }
 
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -3,40 +3,50 @@
 #include <stdlib.h>
 
 /**
- * main - entry point
+ * main - entry point for calculator
  * @argc: number of arguments
  * @argv: array of arguments
  *
- * Return: 0 on success, exits with code 98/99/100 on error
+ * Return: 0 on success,
+ *         98 if wrong arg count,
+ *         99 if invalid operator,
+ *         100 if division/mod by zero
  */
 int main(int argc, char *argv[])
 {
-    int num1, num2;
-    int (*op_func)(int, int);
+	int num1, num2;
+	int (*op_func)(int, int);
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        exit(98);
-    }
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-    op_func = get_op_func(argv[2]);
-    if (!op_func)
-    {
-        printf("Error\n");
-        exit(99);
-    }
+	/* ✅ تحقق أن الـoperator عبارة عن حرف واحد فقط */
+	if (argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[3]);
+	op_func = get_op_func(argv[2]);
+	if (!op_func)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-    if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
-    {
-        printf("Error\n");
-        exit(100);
-    }
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 
-    printf("%d\n", op_func(num1, num2));
-    return (0);
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", op_func(num1, num2));
+	return (0);
 }
 
