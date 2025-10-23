8c43ce6e3910784a85a5bc1356efd526bbbbce0e
Rinadfahadz <rinad-alzuaybir@outlook.com>
2025-10-23 17:34:55 +0000

    msgs

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * check_write_error - checks write errors
+ * check_write_error - check write errors
  * @w: write return value
  * @r: read return value
- * @filename: filename to write
+ * @filename: file to write
  * @fd_from: source fd
  * @fd_to: dest fd
  */
@@ -20,25 +20,7 @@ void check_write_error(int w, int r, char *filename, int fd_from, int fd_to)
 }
 
 /**
- * check_read_error - checks read errors
- * @r: read return value
- * @filename: filename to read
- * @fd_from: source fd
- * @fd_to: dest fd
- */
-void check_read_error(int r, char *filename, int fd_from, int fd_to)
-{
-	if (r == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
-		close(fd_from);
-		close(fd_to);
-		exit(98);
-	}
-}
-
-/**
- * check_close_error - checks close errors
+ * check_close_error - check close errors
  * @fd: file descriptor
  */
 void check_close_error(int fd)
@@ -51,7 +33,59 @@ void check_close_error(int fd)
 }
 
 /**
- * main - copies content of a file to another
+ * open_files - open source and destination files
+ * @file_from: source file
+ * @file_to: destination file
+ * @fd_from: pointer to source fd
+ * @fd_to: pointer to destination fd
+ */
+void open_files(char *file_from, char *file_to, int *fd_from, int *fd_to)
+{
+	*fd_from = open(file_from, O_RDONLY);
+	if (*fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+
+	*fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (*fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		close(*fd_from);
+		exit(99);
+	}
+}
+
+/**
+ * copy_data - copy content from fd_from to fd_to
+ * @fd_from: source file descriptor
+ * @fd_to: destination file descriptor
+ * @file_from: source filename
+ * @file_to: destination filename
+ */
+void copy_data(int fd_from, int fd_to, char *file_from, char *file_to)
+{
+	int r, w;
+	char buffer[1024];
+
+	while ((r = read(fd_from, buffer, 1024)) != 0)
+	{
+		if (r == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+			check_close_error(fd_from);
+			check_close_error(fd_to);
+			exit(98);
+		}
+
+		w = write(fd_to, buffer, r);
+		check_write_error(w, r, file_to, fd_from, fd_to);
+	}
+}
+
+/**
+ * main - copies content of a file to another file
  * @argc: argument count
  * @argv: argument vector
  *
@@ -59,8 +93,7 @@ void check_close_error(int fd)
  */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to, r, w;
-	char buffer[1024];
+	int fd_from, fd_to;
 
 	if (argc != 3)
 	{
@@ -68,28 +101,8 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-
-	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fd_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		check_close_error(fd_from);
-		exit(99);
-	}
-
-	while ((r = read(fd_from, buffer, 1024)) > 0)
-	{
-		w = write(fd_to, buffer, r);
-		check_write_error(w, r, argv[2], fd_from, fd_to);
-	}
-
-	check_read_error(r, argv[1], fd_from, fd_to);
+	open_files(argv[1], argv[2], &fd_from, &fd_to);
+	copy_data(fd_from, fd_to, argv[1], argv[2]);
 	check_close_error(fd_from);
 	check_close_error(fd_to);
 
