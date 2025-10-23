0f97a682aa1640567feda06017c1c6a75c3a900c
Rinadfahadz <rinad-alzuaybir@outlook.com>
2025-10-23 17:40:22 +0000

    m

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,19 +1,40 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#define BUFFER_SIZE 1024
 
 /**
- * check_write_error - check write errors
- * @w: write return value
- * @r: read return value
- * @filename: destination file
+ * check_read_error - check if read failed
+ * @fd_from: source file descriptor
+ * @r: return value of read
+ * @file_name: source file name
+ */
+void check_read_error(int fd_from, ssize_t r, char *file_name)
+{
+	if (r == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_name);
+		if (close(fd_from) == -1)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(98);
+	}
+}
+
+/**
+ * check_write_error - check if write failed
  * @fd_from: source file descriptor
  * @fd_to: destination file descriptor
+ * @w: return value of write
+ * @file_name: destination file name
  */
-void check_write_error(ssize_t w, ssize_t r, char *filename,
-		       int fd_from, int fd_to)
+void check_write_error(int fd_from, int fd_to, ssize_t w, char *file_name)
 {
-	if (w == -1 || w != r)
+	if (w == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_name);
 		if (close(fd_from) == -1)
 			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
 		if (close(fd_to) == -1)
@@ -23,7 +44,7 @@ void check_write_error(ssize_t w, ssize_t r, char *filename,
 }
 
 /**
- * check_close_error - check close errors
+ * check_close_error - check if close failed
  * @fd: file descriptor
  */
 void check_close_error(int fd)
@@ -32,58 +53,6 @@ void check_close_error(int fd)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
-	}
-}
-
-/**
- * open_files - open source and destination files
- * @file_from: source file
- * @file_to: destination file
- * @fd_from: pointer to source fd
- * @fd_to: pointer to destination fd
- */
-void open_files(char *file_from, char *file_to, int *fd_from, int *fd_to)
-{
-	*fd_from = open(file_from, O_RDONLY);
-	if (*fd_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
-
-	*fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (*fd_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-		check_close_error(*fd_from);
-		exit(99);
-	}
-}
-
-/**
- * copy_data - copy content from fd_from to fd_to
- * @fd_from: source file descriptor
- * @fd_to: destination file descriptor
- * @file_from: source filename
- * @file_to: destination filename
- */
-void copy_data(int fd_from, int fd_to, char *file_from, char *file_to)
-{
-	ssize_t r, w;
-	char buffer[1024];
-
-	while ((r = read(fd_from, buffer, 1024)) > 0)
-	{
-		w = write(fd_to, buffer, r);
-		check_write_error(w, r, file_to, fd_from, fd_to);
-	}
-
-	if (r == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-		check_close_error(fd_from);
-		check_close_error(fd_to);
-		exit(98);
 	}
 }
 
@@ -97,6 +66,8 @@ void copy_data(int fd_from, int fd_to, char *file_from, char *file_to)
 int main(int argc, char *argv[])
 {
 	int fd_from, fd_to;
+	ssize_t r, w;
+	char buffer[BUFFER_SIZE];
 
 	if (argc != 3)
 	{
@@ -104,8 +75,30 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 
-	open_files(argv[1], argv[2], &fd_from, &fd_to);
-	copy_data(fd_from, fd_to, argv[1], argv[2]);
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		check_close_error(fd_from);
+		exit(99);
+	}
+
+	while ((r = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		check_read_error(fd_from, r, argv[1]);
+		w = write(fd_to, buffer, r);
+		check_write_error(fd_from, fd_to, w, argv[2]);
+	}
+
+	check_read_error(fd_from, r, argv[1]);
+
 	check_close_error(fd_from);
 	check_close_error(fd_to);
 
